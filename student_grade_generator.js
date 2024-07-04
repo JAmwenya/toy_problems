68fe4ae576fd67f4ad78e7f24c1385d13ae5a1b4
Jackline Amwenya Nehondo <jnehondo@gmail.com>
2024-07-04 17:07:01 +0300

    updates student grade generator to reject input lower than 0 and higher than 100

--- student_grade_generator.js
+++ student_grade_generator.js
@@ -4,20 +4,22 @@ function calculateGrade() {
     let grade;
 
     //checks if an input is provided
-    if (marks !== null) {
+    if (marks !== null && marks !== "") {
     //convert the input to integer
         marks = parseInt(marks, 10);
-        
-        if (marks > 79) {
+
+        if (marks > 79 && marks <= 100) {
             grade = 'A';
-        } else if (marks >= 60) {
+        } else if (marks<= 78 && marks >= 60) {
             grade = 'B';
-        } else if (marks >= 50) {
+        } else if (marks <= 59 && marks >= 50) {
             grade = 'C';
-        } else if (marks >= 40) {
+        } else if (marks <=49 && marks >= 40) {
             grade = 'D';
-        } else {
+        } else if (marks <= 39 && marks >= 0){
             grade = 'E';
+        } else if (marks < 0 || marks > 100){
+            grade = 'Invalid input';
         }
 
     //display the grade
